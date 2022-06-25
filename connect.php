3ef49b4e1479c05c7a0603c0ee5ad88e5b7dd2b9
scorp6969 <scorpdp6969@gmail.com>
2022-06-25 23:20:19 +0545

    database connection updated

--- connect.php
+++ connect.php
@@ -3,37 +3,43 @@
 $host = "localhost";
 $user = "root";
 $pass = "root1234";
-$db = "sample";
+$db = "userinfo";
 
 $email = $_POST['email'];
 $passwd = $_POST['password'];
 
 // process 1
+$con = new mysqli($host,$user,$pass,$db);
+if (!$con) {
+  die('Connection failed: '.mysqli_connect_error());
+}
+else{
+  echo "Successful Connection";
+  
+}
 
-// $con = new mysqli($host,$user,$pass,$db);
-// if (!$con) {
-//   echo "Database not connected!";
-// }
-
-// $qry = "INSERT INTO `idpass` (`e_id`, `email`, `password`) VALUES (NULL, '$email', '$passwd')";
-// $submit = mysqli_query($con, $qry);
-// if(!$submit){
-//   echo "Database not connected!";
-// }else{
-//   echo "Submitted Successfully";
-// }
+$qry = "INSERT INTO `userinfodata` (`id`, `email`, `password`) VALUES (NULL, '$email', '$passwd')";
+$submit = mysqli_query($con, $qry);
+if($submit){
+  echo "New record created successfully";
+  echo "$submit";
+} else {
+  echo "Error: " . $qry . "<br>" . mysqli_error($con);
+}
+mysqli_close($con);
 
 // process 2
-$conn = new mysqli($host,$user,$pass,$db);
-if($conn->connect_error){
-  die('Connection failed: '.$conn->connect_error);
-}else{
-  $stmt = $conn->prepare('INSERT INTO `idpass` (`e_id`, `email`, `password`) VALUES (Null, ?, ?)');
-  $stmt->bind_param("ss", $email, $passwd);
-  $stmt->execute();
-  echo 'Submitted Successfully';
-  $stmt->close();
-  $conn->close();
-}
+// $conn = new mysqli($host,$user,$pass,$db);
+// if($conn->connect_error){
+//   die('Connection failed: '.$conn->connect_error);
+// }
+// else{
+//   $stmt = $conn->prepare("INSERT INTO `userinfodata` (`id`, `email`, `password`) VALUES (Null, ?, ?)");
+//   $stmt->bind_param("ss", $email, $passwd);
+//   $stmt->execute();
+//   echo 'Submitted Successfully';
+//   $stmt->close();
+//   $conn->close();
+// }
 
 ?>

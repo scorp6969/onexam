eb57cda701dacfb664f6d8b2cbcde2277e2b9431
scorp6969 <scorpdp6969@gmail.com>
2022-06-25 23:19:36 +0545

    updated homepage

--- index.php
+++ index.php
@@ -83,20 +83,30 @@
   </div>
 
   <!-- Contact form -->
-  <div class="container">
-    <form action="connect.php" , method="POST">
+  <section>
+    <div class="py-5">
+      <h2 class="text-center">Feedback</h2>
+    </div>
+    <form action="connect.php" , method="POST" class="w-50 m-auto">
       <div class="form-group">
         <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <input type="email" class="form-control" name="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" autocomplete="off">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
+        <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password" autocomplete="off">
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
+      <!-- <div class="alert alert-success" role="alert">
+        This is a success alert—check it out!
+      </div> -->
     </form>
-  </div>
+  </section>
+
+  <footer class="pt-5">
+    <p class="p-3 bg-dark text-white text-center">@online-examination-system</p>
+  </footer>
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
